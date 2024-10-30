6aa043e32081a904a0a1ec2f5b8a21b90ea2e3ce
PeterK <pxk9163@mavs.uta.edu>
2024-10-30 11:48:18 -0500

    Created static Gym Profiles Page with dummy data

--- src/components/GymProfilePage.jsx
+++ src/components/GymProfilePage.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import './GymProfilePage.css';
+import { useNavigate } from 'react-router-dom';
 import Banner from './Banner.jsx';
 
 function GymProfilePage() {
@@ -11,16 +12,59 @@ function GymProfilePage() {
     //Allow user to create new "Gym profile" 
 
     //Display current gym profiles
+    const navigate = useNavigate();
+    const savedGyms = [
+        {
+            id: 1,
+            name: "Home Gym",
+            equipment: ["Treadmill", "Dumbbells", "Bench Press", "Pull-up Bar"]
+        },
+        {
+            id: 2,
+            name: "UTA Gym",
+            equipment: ["Elliptical", "Rowing Machine", "Kettlebells", "Squat Rack"]
+        },
+        {
+            id: 3,
+            name: "Hotel Gym",
+            equipment: ["Leg Press", "Cable Machine", "Spin Bikes", "Battle Ropes"]
+        }
+    ];
 
-    //in Banner.jsx add homebutton opttion
+    const handleSelectGym = (gym) => {
+        navigate('/gymProfile', { state: { gymName: gym.name, equipment: gym.equipment } });
+    };
 
     return (
         <div className="gymprofile-container">
+
             <Banner text="Gym Profiles Page"></Banner>
-            <div>
+
+            <div className="button-container-gym-profiles">
                 <button>Create New Gym</button>
             </div>
+
+            <div className="user-gyms-dropdown">
+                <h1>My Gyms</h1>
+                {savedGyms.length > 0 ? (
+                    <div className="gym-list">
+                        {savedGyms.map((gym) => (
+                            <li
+                                key={gym.id}
+                                className="gym-item-button"
+                                onClick={() => handleSelectGym(gym)}
+                            >
+                            <img src="/src/local_assets/weightIcon.png" alt="gym icon" className="gym-icon" /> 
+                            <span>{gym.name}</span>
+                            </li>
+                        ))}
+                    </div>
+                ) : (
+                    <h1>No Gyms Found</h1>
+                )}
+            </div>
         </div>
+
     );
 }
 
